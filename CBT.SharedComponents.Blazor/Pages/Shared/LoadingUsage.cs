963a90f764be6ed66e5aa28af32926a6e5d7e0f3
electrionics <davidlewin.dk@gmail.com>
2024-11-09 15:17:34 +0300

    Fix texts (lack of title on pages). Add spinner when loading data and integrate it in existing components.

---
+++ CBT.SharedComponents.Blazor/Pages/Shared/LoadingUsage.cs
@@ -0,0 +1,19 @@
+﻿namespace CBT.SharedComponents.Blazor.Pages.Shared
+{
+    public class LoadingUsage(Loading loading) : IAsyncDisposable
+    {
+        private readonly Loading loading = loading;
+
+        public async Task<LoadingUsage> StartAsync()
+        {
+            await loading.StartAsync();
+
+            return this;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await loading.StopAsync();
+        }
+    }
+}
